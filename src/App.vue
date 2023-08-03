3cdaa1410238e9c245fa7d6b7041d9c8e700bb75
luan.indra <llucasb@minsait.com>
2023-08-02 22:12:40 -0300

    feat: Implementação contador de segundos para tarefas

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
-  <main>
-    
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral/>
+    </div>
+    <div class="column is-three-quarter">
+      <FormularioTarefas/>
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
+import BarraLateral from './components/BarraLateral.vue';
+import FormularioTarefas from './components/FormularioTarefas.vue'
 export default defineComponent({
   name: 'App',
+   components: {
+    BarraLateral,
+    FormularioTarefas
+   }
 });
 </script>
 
