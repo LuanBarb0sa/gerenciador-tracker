732621e2ba900fb7a6091474335db18aa5ae56b2
luan.indra <llucasb@minsait.com>
2023-08-06 11:41:30 -0300

    feat: Componentizando componentes.

--- src/components/FormularioTarefas.vue
+++ src/components/FormularioTarefas.vue
@@ -13,55 +13,19 @@
         />
       </div>
       <div class="column">
-        <div
-          class="is-flex is-align-items-center is-justify-content-space-between"
-        >
-          <section>
-            <strong>{{ tempoDecorrido }}</strong>
-          </section>
-          <button class="button ml-3" @click="iniciar">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-          <button class="button ml-3" @click="finalizar">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
+        <TemporizadorTarefas/>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
+import TemporizadorTarefas from '../components/TemporizadorTarefas.vue'
 export default defineComponent({
   name: "FormulárioTarefas",
-  data() {
-    return {
-      tempoEmSegundos: 0,
-      cronometro: 0
-    };
-  },
-  computed: {
-    tempoDecorrido () : string {
-      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8);
-    },
-  },
-  methods: {
-    iniciar() {
-      // começar a contagem
-      this.cronometro = setInterval(() => {
-        this.tempoEmSegundos++;
-      }, 1000);
-    },
-    finalizar() {
-      clearInterval(this.cronometro)
-    },
-  },
+  components: {
+    TemporizadorTarefas
+  }
 });
 </script>
 <style></style>
---
+++ src/components/TemporizadorTarefas.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <CronometroTarefas :tempoEmSegundos="tempoEmSegundos" />
+    <button class="button ml-3" @click="iniciar" :disabled="cronometroRodando">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+    <button class="button ml-3" @click="finalizar" :disabled="!cronometroRodando">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import CronometroTarefas from '../components/CronometroTarefas.vue'
+export default defineComponent({
+  name: "TemporizadorTarefas",
+  components: {
+    CronometroTarefas
+  },
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0,
+      cronometroRodando: false
+    };
+  },
+  methods: {
+    iniciar() {
+    this.cronometroRodando = true
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos++;
+      }, 1000);
+    },
+    finalizar() {
+        this.cronometroRodando = false
+      clearInterval(this.cronometro)
+    },
+  },
+});
+</script>
+<style></style>
