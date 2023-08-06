e3e740d17a9d74c4603b0edd6e228e8c9699f46d
luan.indra <llucasb@minsait.com>
2023-08-06 15:48:25 -0300

    chore: Fim implementação modo escuro.

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,16 +1,38 @@
 <template>
-    <header>
-        <h1>
-            <img src="../assets/logo.png" alt="">
-        </h1>
-    </header>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="" />
+    </h1>
+    <button class="button" @click="alterarTema">{{ textoBotaoTema }}</button>
+  </header>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: 'BarraLateral'
-})
+  name: "BarraLateral",
+  emits: ["aoTemaAlterado"],
+  data() {
+    return {
+      modoEscuroAtivo: false,
+    };
+  },
+  computed: {
+    textoBotaoTema () {
+      if (this.modoEscuroAtivo) {
+        return 'Desativar modo escuro'
+      } else {
+        return 'Ativar modo escuro'
+      }
+    },
+  },
+  methods: {
+    alterarTema() {
+      this.modoEscuroAtivo = !this.modoEscuroAtivo;
+      this.$emit("aoTemaAlterado", this.modoEscuroAtivo);
+    },
+  },
+});
 </script>
 <style scoped>
 header {
@@ -18,6 +40,7 @@ header {
   background: #0d3b66;
   width: 100%;
   height: 100vh;
+  text-align: center;
 }
 @media only screen and (max-width: 768px) {
   header {
